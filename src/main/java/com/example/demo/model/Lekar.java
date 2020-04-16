5e72310c0626028ba98ae1e7e5d03747f6bc46a1
draganagrbic98 <dragana.grbic.98@uns.ac.rs>
2020-04-16 22:24:09 +0200

    Ponovno testiranje deployment-a.

--- src/main/java/com/example/demo/model/Lekar.java
+++ src/main/java/com/example/demo/model/Lekar.java
@@ -99,141 +99,141 @@ public class Lekar extends Zaposleni implements Ocenjivanje{
 		
 	}
 	
-//	public List<Date> getSatnica(Date datum) {
-//
-//		List<Date> satnica = new ArrayList<>();
-//		if (datum == null)
-//			return satnica;
-//		
-//		for  (ZahtevOdmor z: this.odmorZahtevi) {
-//			if (z.getOdobren() && (z.getPocetak().equals(datum) || z.getPocetak().before(datum)) && (z.getKraj().equals(datum) || z.getKraj().after(datum)))
-//				return satnica;
-//		}
-//		
-//
-//		Calendar kalendar = Calendar.getInstance();
-//		kalendar.setTime(datum);
-//		kalendar.set(Calendar.HOUR_OF_DAY, 0);
-//		kalendar.set(Calendar.MINUTE, 0);
-//		Date compareDate = kalendar.getTime();
-//		
-//		List<Zauzetost> lista = new ArrayList<>();
-//		for (Poseta p: this.posete) {
-//			kalendar.setTime(p.datum());
-//			kalendar.set(Calendar.HOUR_OF_DAY, 0);
-//			kalendar.set(Calendar.MINUTE, 0);
-//			if (kalendar.getTime().equals(compareDate) && !p.getStanje().equals(StanjePosete.OBAVLJENO))
-//				lista.add(p);
-//		}
-//		for (ZahtevPregled zp: this.pregledZahtevi) {
-//			kalendar.setTime(zp.datum());
-//			kalendar.set(Calendar.HOUR_OF_DAY, 0);
-//			kalendar.set(Calendar.MINUTE, 0);
-//			if (kalendar.getTime().equals(compareDate))
-//				lista.add(zp);
-//
-//
-//		}
-//		
-//		
-//		Collections.sort(lista);
-//
-//		
-//		kalendar.setTime(this.getPocetnoVreme());
-//		int sati = kalendar.get(Calendar.HOUR_OF_DAY);
-//		int minute = kalendar.get(Calendar.MINUTE);
-//		kalendar.setTime(datum);
-//		kalendar.set(Calendar.HOUR_OF_DAY, sati);
-//		kalendar.set(Calendar.MINUTE, minute);
-//		Date start = kalendar.getTime();
-//
-//		kalendar.setTime(this.getKrajnjeVreme());
-//		sati = kalendar.get(Calendar.HOUR_OF_DAY);
-//		minute = kalendar.get(Calendar.MINUTE);
-//		kalendar.setTime(datum);
-//		kalendar.set(Calendar.HOUR_OF_DAY, sati);
-//		kalendar.set(Calendar.MINUTE, minute);
-//		Date end = kalendar.getTime();
-//
-//		Iterator<Zauzetost> iterator = lista.iterator();
-//		Zauzetost p = null;
-//		boolean sledeci = true;
-//
-//		while (true) {
-//			
-//			kalendar.setTime(start);
-//			kalendar.add(Calendar.HOUR_OF_DAY, this.specijalizacija.getSati());
-//			kalendar.add(Calendar.MINUTE, this.specijalizacija.getMinute());
-//			Date temp1 = kalendar.getTime();
-//
-//			if (temp1.after(end))
-//				break;
-//
-//			
-//			if (sledeci) {
-//				
-//				if (!iterator.hasNext()) {
-//					
-//					while (temp1.before(end) || temp1.equals(end)) {
-//						
-//						satnica.add((Date) start.clone());
-//						kalendar.setTime(start);
-//						kalendar.add(Calendar.MINUTE, 30);
-//						start = kalendar.getTime();
-//						kalendar.setTime(start);
-//						kalendar.add(Calendar.HOUR_OF_DAY, this.specijalizacija.getSati());
-//						kalendar.add(Calendar.MINUTE, this.specijalizacija.getMinute());
-//						temp1 = kalendar.getTime();
-//
-//					}
-//					
-//					break;
-//				}
-//				
-//				p = iterator.next();
-//				
-//			}
-//			
-//			kalendar.setTime(p.datum());
-//			kalendar.add(Calendar.HOUR_OF_DAY, p.sati());
-//			kalendar.add(Calendar.MINUTE, p.minute());
-//			Date temp2 = kalendar.getTime();
-//			
-//			if ((start.after(p.datum()) || start.equals(p.datum())) && (start.before(temp2))) {
-//				kalendar.setTime(temp2);
-//				start = kalendar.getTime();
-//				sledeci = true;
-//
-//			}
-//			
-//			else if ((temp1.after(p.datum())) && (temp1.before(temp2) || temp1.equals(temp2))) {
-//				kalendar.setTime(temp2);
-//				start = kalendar.getTime();
-//				sledeci = true;
-//
-//			}
-//			
-//			else if (start.before(p.datum()) && temp1.after(temp2)) {
-//				kalendar.setTime(temp2);
-//				start = kalendar.getTime();
-//				sledeci = true;
-//
-//			}
-//			
-//
-//
-//			
-//			else {
-//				satnica.add((Date) start.clone());
-//				kalendar.setTime(start);
-//				kalendar.add(Calendar.MINUTE, 30);
-//				start = kalendar.getTime();
-//				sledeci = false;
-//			}	
-//		}
-//
-//		return satnica;
-//		
-//	}
+	public List<Date> getSatnica(Date datum) {
+
+		List<Date> satnica = new ArrayList<>();
+		if (datum == null)
+			return satnica;
+		
+		for  (ZahtevOdmor z: this.odmorZahtevi) {
+			if (z.getOdobren() && (z.getPocetak().equals(datum) || z.getPocetak().before(datum)) && (z.getKraj().equals(datum) || z.getKraj().after(datum)))
+				return satnica;
+		}
+		
+
+		Calendar kalendar = Calendar.getInstance();
+		kalendar.setTime(datum);
+		kalendar.set(Calendar.HOUR_OF_DAY, 0);
+		kalendar.set(Calendar.MINUTE, 0);
+		Date compareDate = kalendar.getTime();
+		
+		List<Zauzetost> lista = new ArrayList<>();
+		for (Poseta p: this.posete) {
+			kalendar.setTime(p.datum());
+			kalendar.set(Calendar.HOUR_OF_DAY, 0);
+			kalendar.set(Calendar.MINUTE, 0);
+			if (kalendar.getTime().equals(compareDate) && !p.getStanje().equals(StanjePosete.OBAVLJENO))
+				lista.add(p);
+		}
+		for (ZahtevPregled zp: this.pregledZahtevi) {
+			kalendar.setTime(zp.datum());
+			kalendar.set(Calendar.HOUR_OF_DAY, 0);
+			kalendar.set(Calendar.MINUTE, 0);
+			if (kalendar.getTime().equals(compareDate))
+				lista.add(zp);
+
+
+		}
+		
+		
+		Collections.sort(lista);
+
+		
+		kalendar.setTime(this.getPocetnoVreme());
+		int sati = kalendar.get(Calendar.HOUR_OF_DAY);
+		int minute = kalendar.get(Calendar.MINUTE);
+		kalendar.setTime(datum);
+		kalendar.set(Calendar.HOUR_OF_DAY, sati);
+		kalendar.set(Calendar.MINUTE, minute);
+		Date start = kalendar.getTime();
+
+		kalendar.setTime(this.getKrajnjeVreme());
+		sati = kalendar.get(Calendar.HOUR_OF_DAY);
+		minute = kalendar.get(Calendar.MINUTE);
+		kalendar.setTime(datum);
+		kalendar.set(Calendar.HOUR_OF_DAY, sati);
+		kalendar.set(Calendar.MINUTE, minute);
+		Date end = kalendar.getTime();
+
+		Iterator<Zauzetost> iterator = lista.iterator();
+		Zauzetost p = null;
+		boolean sledeci = true;
+
+		while (true) {
+			
+			kalendar.setTime(start);
+			kalendar.add(Calendar.HOUR_OF_DAY, this.specijalizacija.getSati());
+			kalendar.add(Calendar.MINUTE, this.specijalizacija.getMinute());
+			Date temp1 = kalendar.getTime();
+
+			if (temp1.after(end))
+				break;
+
+			
+			if (sledeci) {
+				
+				if (!iterator.hasNext()) {
+					
+					while (temp1.before(end) || temp1.equals(end)) {
+						
+						satnica.add((Date) start.clone());
+						kalendar.setTime(start);
+						kalendar.add(Calendar.MINUTE, 30);
+						start = kalendar.getTime();
+						kalendar.setTime(start);
+						kalendar.add(Calendar.HOUR_OF_DAY, this.specijalizacija.getSati());
+						kalendar.add(Calendar.MINUTE, this.specijalizacija.getMinute());
+						temp1 = kalendar.getTime();
+
+					}
+					
+					break;
+				}
+				
+				p = iterator.next();
+				
+			}
+			
+			kalendar.setTime(p.datum());
+			kalendar.add(Calendar.HOUR_OF_DAY, p.sati());
+			kalendar.add(Calendar.MINUTE, p.minute());
+			Date temp2 = kalendar.getTime();
+			
+			if ((start.after(p.datum()) || start.equals(p.datum())) && (start.before(temp2))) {
+				kalendar.setTime(temp2);
+				start = kalendar.getTime();
+				sledeci = true;
+
+			}
+			
+			else if ((temp1.after(p.datum())) && (temp1.before(temp2) || temp1.equals(temp2))) {
+				kalendar.setTime(temp2);
+				start = kalendar.getTime();
+				sledeci = true;
+
+			}
+			
+			else if (start.before(p.datum()) && temp1.after(temp2)) {
+				kalendar.setTime(temp2);
+				start = kalendar.getTime();
+				sledeci = true;
+
+			}
+			
+
+
+			
+			else {
+				satnica.add((Date) start.clone());
+				kalendar.setTime(start);
+				kalendar.add(Calendar.MINUTE, 30);
+				start = kalendar.getTime();
+				sledeci = false;
+			}	
+		}
+
+		return satnica;
+		
+	}
 	
 }
