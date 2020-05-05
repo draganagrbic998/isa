c7ed846081c6cd6e9c23356f679842bd28a3b81d
draganagrbic98 <dragana.grbic.98@uns.ac.rs>
2020-05-05 02:18:15 +0200

    Ponovno testiranje novih podesavanja SonarCloud alata.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -10,6 +10,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 public class DemoApplication {
 	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class);
 		
