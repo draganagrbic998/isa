03012500d70fed4edfbe3e00aa38b4c306f84b18
draganagrbic98 <dragana.grbic.98@uns.ac.rs>
2020-05-09 12:43:51 +0200

    Sitna izmena.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -12,6 +12,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableScheduling
 public class DemoApplication {
 	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class);	
 	}
